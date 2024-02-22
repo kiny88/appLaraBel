a9e84eb1c33dfd8f9e9efb5ab5f5d846dc0d44e1
kiny88 <j.rodriguez.ladera@gmail.com>
2024-02-22 18:32:47 +0100

    cambio email

--- classes/Email.php
+++ classes/Email.php
@@ -28,8 +28,8 @@ class Email{
         $mail->Password = $_ENV['EMAIL_PASS'];
 
         // Configurar el contenido del email
-        $mail->setFrom('cuentas@appsalon.com');
-        $mail->addAddress('cuentas@appsalon.com','AppSalon.com');
+        $mail->setFrom('cuentas@larabel.com');
+        $mail->addAddress('cuentas@larabel.com','LaraBel.com');
         $mail->Subject = 'Confirma tu cuenta';
 
         // Habilitar HTML
@@ -38,7 +38,7 @@ class Email{
 
         // Definir el contenido
         $contenido = "<html>";
-        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has creado tu cuenta en App Salón, solo debes confirmarla presionando el siguiente enlace</p>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has creado tu cuenta en LaraBel Centro de Estética, solo debes confirmarla presionando el siguiente enlace</p>";
         $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['APP_URL'] . "/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
         $contenido .= "</html>";
@@ -60,9 +60,9 @@ class Email{
         $mail->Password = $_ENV['EMAIL_PASS'];
 
         // Configurar el contenido del email
-        $mail->setFrom('cuentas@appsalon.com');
-        $mail->addAddress('cuentas@appsalon.com','AppSalon.com');
-        $mail->Subject = 'Reestablece tu password';
+        $mail->setFrom('cuentas@larabel.com');
+        $mail->addAddress('cuentas@larabel.com','LaraBel.com');
+        $mail->Subject = 'Reestablece tu contraseña';
 
         // Habilitar HTML
         $mail->isHTML(TRUE);
@@ -70,8 +70,8 @@ class Email{
 
         // Definir el contenido
         $contenido = "<html>";
-        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
-        $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['APP_URL'] . "/recuperar?token=" . $this->token . "'>Reestablecer Password</a></p>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has solicitado reestablecer tu contraseña, sigue el siguiente enlace para hacerlo.</p>";
+        $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['APP_URL'] . "/recuperar?token=" . $this->token . "'>Reestablecer Contraseña</a></p>";
         $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
         $contenido .= "</html>";
 
