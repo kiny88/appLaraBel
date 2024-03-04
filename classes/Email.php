319254fae4480a02ba083939060a5433acc972e6
kiny88 <j.rodriguez.ladera@gmail.com>
2024-03-04 18:58:01 +0100

    quitar todos los comentarios de las funciones de phpMailer

--- classes/Email.php
+++ classes/Email.php
@@ -2,7 +2,6 @@
 
 namespace Classes;
 
-use PHPMailer\PHPMailer\PHPMailer;
 use Resend;
 
 class Email{
@@ -19,35 +18,6 @@ class Email{
 
     // Enivar confirmación del email
     public function enviarConfirmacion(){
-        // Crear el objeto de email
-        /*$mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->Host = $_ENV['EMAIL_HOST'];
-        $mail->SMTPAuth = true;
-        $mail->Port = $_ENV['EMAIL_PORT'];
-        $mail->Username = $_ENV['EMAIL_USER'];
-        $mail->Password = $_ENV['EMAIL_PASS'];
-
-        // Configurar el contenido del email
-        $mail->setFrom('info@larabel.com');
-        $mail->addAddress($this->email,$this->nombre);
-        $mail->Subject = 'Confirma tu cuenta';
-
-        // Habilitar HTML
-        $mail->isHTML(TRUE);
-        $mail->CharSet = 'UTF-8';
-
-        // Definir el contenido
-        $contenido = "<html>";
-        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has creado tu cuenta en LaraBel Centro de Estética, solo debes confirmarla presionando el siguiente enlace</p>";
-        $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['APP_URL'] . "/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
-        $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
-        $contenido .= "</html>";
-
-        $mail->Body = $contenido;
-
-        // Enviar el email
-        $mail->send();*/
         $resend = Resend::client('re_5x1UfH6x_LNeFByjwm5t8g9WQWLU8p8cN');
         
         $resend->emails->send([
@@ -61,35 +31,6 @@ class Email{
     }
 
     public function enviarInstrucciones(){
-        // Crear el objeto de email
-        /*$mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->Host = $_ENV['EMAIL_HOST'];
-        $mail->SMTPAuth = true;
-        $mail->Port = $_ENV['EMAIL_PORT'];
-        $mail->Username = $_ENV['EMAIL_USER'];
-        $mail->Password = $_ENV['EMAIL_PASS'];
-
-        // Configurar el contenido del email
-        $mail->setFrom('cuentas@larabel.com');
-        $mail->addAddress($this->email,$this->nombre);
-        $mail->Subject = 'Reestablece tu contraseña';
-
-        // Habilitar HTML
-        $mail->isHTML(TRUE);
-        $mail->CharSet = 'UTF-8';
-
-        // Definir el contenido
-        $contenido = "<html>";
-        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has solicitado reestablecer tu contraseña, sigue el siguiente enlace para hacerlo.</p>";
-        $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['APP_URL'] . "/recuperar?token=" . $this->token . "'>Reestablecer Contraseña</a></p>";
-        $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
-        $contenido .= "</html>";
-
-        $mail->Body = $contenido;
-
-        // Enviar el email
-        $mail->send();*/
         $resend = Resend::client('re_5x1UfH6x_LNeFByjwm5t8g9WQWLU8p8cN');
         
         $resend->emails->send([
